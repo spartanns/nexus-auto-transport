0d03cbacfad881c0db5936749b110a0e63bdd0a8
Milos Spartan <milos.djurdjevic.brains24@gmail.com>
2024-11-25 23:54:40 +0100

    More dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <script setup>
+import { ref, onMounted } from "vue";
+
+const option1 = ref("");
+const option2 = ref("");
+const option3 = ref("");
+
+const years = ref([]);
+const makes = ref([]);
+const models = ref([]);
 </script>
 
 <template>
-  <h1>Nexus Auto Transport</h1>
+  <div class="bg-slate-800 h-screen w-full flex justify-center items-center gap-4 border border-gray-300 p-4">
+  </div>
 </template>
