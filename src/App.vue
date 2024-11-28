d69695b848d480b8c96133d52c72d9c7ddde1c26
Milos Spartan <milos.djurdjevic.brains24@gmail.com>
2024-11-28 23:35:57 +0100

    Finalize

--- src/App.vue
+++ src/App.vue
@@ -20,24 +20,33 @@ onMounted(async () => {
 });
 
 const fetchMakes = async () => {
-  const res = await fetch(`https://new.api.nexusautotransport.com/api/vehicles/makes?year=${option1.value}`);
-  const data = await res.json();
-  makes.value = data.data;
+  try {
+    const res = await fetch(`https://new.api.nexusautotransport.com/api/vehicles/makes?year=${option1.value}`);
+    const data = await res.json();
+    makes.value = data.data;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 const fetchModels = async () => {
-  const res = await fetch(`https://new.api.nexusautotransport.com/api/vehicles/models?year=${option1.value}&make=${option2.value.name}`);
-  const data = await res.json();
-  models.value = data.data;
+  try {
+    const res = await fetch(`https://new.api.nexusautotransport.com/api/vehicles/models?year=${option1.value}&make=${option2.value.name}`);
+    const data = await res.json();
+    models.value = data.data;
+  } catch (err) {
+    console.log(err);
+  }
 }
 </script>
 
 <template>
-  <div class="bg-slate-800 h-screen w-full flex justify-center items-center gap-4 border border-gray-300 p-4">
+  <div
+    class="bg-slate-800 h-screen w-full flex md:flex-col lg:flex-row  justify-center items-center gap-4 border border-gray-300 p-4">
     <div class="w-full">
       <label for="select1" class="block text-sm font-medium text-slate-200">Year</label>
       <select id="select1" v-model="option1" @change="fetchMakes"
-        class="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+        class="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-[30px]">
         <option disabled value="">Please select a year</option>
         <option v-for="option in years" :key="option" :value="option">{{ option }}</option>
       </select>
@@ -46,7 +55,7 @@ const fetchModels = async () => {
     <div class="w-full">
       <label for="select2" class="block text-sm font-medium text-slate-200">Make</label>
       <select id="select2" v-model="option2" @change="fetchModels" :disabled="!option1"
-        class="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+        class="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-[30px]">
         <option disabled value="">Please select a make</option>
         <option v-for="option in makes" :key="option" :value="option">{{ option.name }}</option>
       </select>
@@ -55,7 +64,7 @@ const fetchModels = async () => {
     <div class="w-full">
       <label for="select3" class="block text-sm font-medium text-slate-200">Model</label>
       <select id="select3" v-model="option3" :disabled="!option2"
-        class="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+        class="block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-[30px]">
         <option disabled value="">Please select a model</option>
         <option v-for="option in models" :key="option" :value="option">{{ option.model }}</option>
       </select>
